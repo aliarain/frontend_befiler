2ac10b4dbcccc1b8f2a19a38269d5f1bbf6d8552
Ali Arain <deistali2@gmail.com>
2025-04-16 19:54:29 -0700

    Config Changed

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ module.exports = {
     env: {
         BACKEND_URL: 
         // 'https://7fab-175-107-225-118.ngrok-free.app'
-        // 'https://befilerbackend-9135r.kinsta.app/'
+        'https://befilerbackend-9135r.kinsta.app/'
         
         // process.env.NODE_ENV === 'development' ?
         //     'https://befilerbackend-9135r.kinsta.app/' : 
-            'http://localhost:5000/'
+            // 'http://localhost:5000/'
     },
     images: {
         domains: ['appstick.s3.ap-southeast-1.amazonaws.com','appstick-resources.s3.ap-southeast-1.amazonaws.com'], 
