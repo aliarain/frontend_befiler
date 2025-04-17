f1c0a0a56ad60e3a0821c0620ac51f6180699253
Ali Arain <deistali2@gmail.com>
2025-04-16 19:06:29 -0700

    Config changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://taxstick.appstick.com.bd/',
+    siteUrl: 'https://befilerbackend-9135r.kinsta.app/',
     generateRobotsTxt: true, // (optional)
     outDir: './out'
 }
